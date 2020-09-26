371db12fd0842b180065bfeb2af1c0677a335a93
ea103 <doreenhsiaxx@gmail.com>
2020-09-26 15:23:38 +0800

    22

--- Hello2.java
+++ Hello2.java
@@ -4,7 +4,7 @@ public class HelloEclipse {
 	
 
     public static void main(String[] args) {
-    	double i_1=10;
+    	double i_1=11;
     	
 //    	System.out.println("hello");
 ////    	System.out.println("test");
