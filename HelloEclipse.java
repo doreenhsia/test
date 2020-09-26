a313632974300863cb83adddb70197c1bc0abfa2
ea103 <doreenhsiaxx@gmail.com>
2020-09-26 13:42:08 +0800

    First version

---
+++ HelloEclipse.java
@@ -0,0 +1,27 @@
+package xxx;
+
+public class HelloEclipse {
+	
+	//������
+	
+	/* �h
+	 * ��
+	 * �`
+	 * ��
+	 */
+	
+    public static void main(String[] args) {
+    	int i_1=10;
+    	
+//    	System.out.println("hello");
+////    	System.out.println("test");
+//    	System.out.println('a');
+    	
+    	String s = "Hello World";
+    	char [] c = s.toCharArray();
+    	for(int i = 0; i < c.length; i++) {
+    		System.out.println((int)c[i]);
+    	}
+    	
+    }
+}
